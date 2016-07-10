88004982184f6f2e33e9c9456227cdece1b079d3
Julian Fischer <julian.fischer184@gmail.com>
2016-07-10 19:55:54 +0200

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Jul 10 18:47:31 CEST 2016"
+#define LINUX_COMPILE_BY "jfischer"
+#define LINUX_COMPILE_HOST "srv-dev"
+#define LINUX_COMPILER "gcc version 4.9.4 20160401 (optimized) (UBERTC-4.9.4) "
